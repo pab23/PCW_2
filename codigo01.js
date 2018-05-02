87a7ca9955fca89ff05fd501b50928b74eaf1156
pcw <pab23@gcloud.ua.es>
2018-05-02 18:54:33 +0200

    Avances en registro

--- codigo01.js
+++ codigo01.js
@@ -57,36 +57,39 @@ function busqueda_rapida(frm){
 	location.href='buscar.html';
 }
 function ultimasRecetas(){
+	var num = 6;
 	let xhr = new XMLHttpRequest(), 
-	url = 'rest/receta/?u=6';
+	url = 'rest/receta/?u='+num;
+	console.log(url);
 	xhr.open( 'GET', url, true);
 	xhr.onload = function(){
 		let objJSON = JSON.parse(xhr.responseText);
-			creaReceta(objJSON);
+			creaReceta(objJSON, num);
 			};
 	xhr.onerror = function(){
 		console.log('ERROR');
 	};
 
+	
 	xhr.send();
-
 }
 
-function creaReceta(recetas){
-	var num_recetas = 6,
-		etiqueta = document.getElementById('cuerpo_receta');
+function creaReceta(recetas, num_recetas){
+		var etiqueta = document.getElementById('cuerpo_receta'),
+			i;
 
-	for(let i=0; i<num_recetas; i++){
+	for(i=0; i<num_recetas; i++){
 		
-		var titulo = recetas.FILAS[total_recetas].nombre,
-			autor = recetas.FILAS[total_recetas].autor,
-			comentario = recetas.FILAS[total_recetas].comentarios,
-			posi = recetas.FILAS[total_recetas].positivos,
-			nega = recetas.FILAS[total_recetas].negativos,
-			foto = recetas.FILAS[total_recetas].fichero,
-			descripcion = recetas.FILAS[total_recetas].descripcion_foto,
-			fecha = recetas.FILAS[total_recetas].fecha,
-			id = recetas.FILAS[total_recetas].id;
+
+		var titulo = recetas.FILAS[i].nombre,
+			autor = recetas.FILAS[i].autor,
+			comentario = recetas.FILAS[i].comentarios,
+			posi = recetas.FILAS[i].positivos,
+			nega = recetas.FILAS[i].negativos,
+			foto = recetas.FILAS[i].fichero,
+			descripcion = recetas.FILAS[i].descripcion_foto,
+			fecha = recetas.FILAS[i].fecha,
+			id = recetas.FILAS[i].id;
 
 
 		var receta = 
@@ -103,14 +106,229 @@ function creaReceta(recetas){
 
 			etiqueta.innerHTML += receta;
 			total_recetas++;
-
 	}
+	ultimaPagina();
+	paginaActual();
+
 }
 
 function ultimaPagina(){
-	let pagina = document.getElementById('pagina'),
-	calcu = Math.ceil((total_recetas)/6);
+	let pagina = document.getElementById('ult_pagina');
 
-	pagina.innerHTML = calcu;
-	pagina_actual = calcu;
+	pagina.innerHTML = Math.ceil((total_recetas)/6);
+	pagina_actual = Math.ceil((total_recetas)/6);
+}
+
+function paginaActual(){
+	let pagina = document.getElementById('pagina_actual');
+
+	pagina.innerHTML = pagina_actual;
+}
+
+function nextPagina(){
+	let pagina = document.getElementById('next_page');
+	pagina_actual++;
+
+}
+
+function nuevoIngrediente(){
+	let ingredientes =document.getElementById('ingrediente');
+
+	ingredientes.innerHTML+='<li>'+ingredientes2.value+'</li>';
+	ingredientes2.value='';
+}
+function anadirFoto(){
+
+}
+
+/*function buscando(){
+	var url_string = window.location.href,
+		url = new URL(url_string),
+		usuario = url.searchParams.get('a'),
+		submit = url.searchParams.get('submit'),
+		pagina = url.searchParams.get('pag');
+	let xhr = new XMLHttpRequest();
+
+		if (usuario != undefined){
+			var url_usr = 'rest/receta/?a='+usuario;
+			xhr.open('GET', url_usr,  true);
+			xhr.onload = function(){
+				let objJSON = JSON.parse(xhr.responseText);
+				creaReceta(objJSON, 6);
+			};
+			xhr.onerror = function(){
+				console.log('ERROR');
+			};
+
+		}else if( (submit != undefined) || (typeof(pagina) != "undefined" && pagina !== null)){
+    // Si usuari ha rellena formulario O pagina
+    var titulo = url.searchParams.get("busqueda");
+    console.log("titulo: ");
+    console.log(titulo);
+    var ingredientes = url.searchParams.get("ingredientes");
+    console.log("ingredientes: ");
+    console.log(ingredientes);
+    var tmin = url.searchParams.get("tiempomin");
+    console.log("tmin: ");
+    console.log(tmin);
+    var tmax = url.searchParams.get("tiempomax");
+    console.log("tmax: ");
+    console.log(tmax);
+    var dificultad = url.searchParams.get("dificultad");
+    console.log("difi: ");
+    console.log(dificultad);
+    var comensales = url.searchParams.get("comensales");
+    console.log("comensales: ");
+    console.log(comensales);
+    var autor = url.searchParams.get("autor");
+    console.log("autor: ");
+    console.log(autor);
+    if( ( submit=="Buscar" && titulo=="" && ingredientes=="" && tmin=="" && tmax=="" && dificultad=="" && comensales=="" && autor=="")){
+      // Usuario no ha introducido ningun valor en la busqueda
+      ultimasRecetas();
+    	}
+	}else{
+		var busca ='rest/receta/?',
+			parametros = [];
+
+		if( typeof(titulo)!='undefined' && titulo!=null && titulo!=''){
+			parametros.t = titulo.replace(/\s+/g,',');
+		}
+		if(typeof(ingredientes) != "undefined" && ingredientes !== null && ingredientes != "" ){
+          params.i = ingredientes.replace(/\s+/g, ',');
+        }
+
+        if(typeof(tmin) != "undefined" && tmin !== null && tmin != "" ){
+          params.di = tmin;
+        }
+
+        if(typeof(tmax) != "undefined" && tmax !== null && tmax != "" ){
+          params.df = tmax;
+        }
+
+        if(typeof(dificultad) != "undefined" && dificultad !== null && dificultad != "" ){
+          params.d = dificultad;
+        }
+
+        if(typeof(comensales) != "undefined" && comensales !== null && comensales != "" ){
+          params.c = comensales;
+        }
+
+        if(typeof(autor) != "undefined" && autor !== null && autor != "" ){
+          params.a = autor;
+        }
+        parametros.pag=0;
+        parametros.lpag=4;
+        if( typeof(pagina)!='undefined' && pagina!=null){
+        	parametros.pag=pagina;
+        	var records = url.searchParams.get('lpag');
+        	if(typeof(records)!='undefined' && records!=null){
+        		parametros.lpag=records;
+        	}
+        }
+        var url_parametriza = build_params(parametros).replace(/\%2C/g,',');
+        busca += url_parametriza;
+
+        xhr.open('GET', busca, true);
+        xhr.onload = function(){
+				let objJSON = JSON.parse(xhr.responseText);
+				creaReceta(objJSON, 6);
+			};
+			xhr.onerror = function(){
+				console.log('ERROR');
+		};
+	}
+}
+
+function build_params(data){
+	let ret = [];
+	for (let pos in data){
+		ret.push(encodeURIComponent(pos)+'='+encodeURIComponent(data[pos]));
+	}
+	return ret.join('&');
+}*/
+
+function comprobarUsuario(valor){
+	let xhr = new XMLHttpRequest(),
+		url = 'rest/login/'+valor;
+
+		xhr.open('GET', url, true);
+
+		xhr.onload = function(){
+			let obj= JSON.parse(xhr.responseText);
+
+			if(obj.RESULTADO=='OK'){
+				var ultado = document.getElementById('estado_usuario');
+				if(obj.DISPONIBLE){
+					ultado.innerHTML="Usuario disponible";
+					ultado.style.color='#05A922';
+				}else{
+					ultado.innerHTML='Usuario no disponible';
+					ultado.style.color='#FF0000';
+				}
+			}else{
+				document.getElementById('estado_usuario').innerHTML='';
+			}
+		}
+		xhr.send();
+		return false;
+}
+
+function registro(formulario){
+	let frm = new FormData(formulario),
+		xhr = new XMLHttpRequest(),
+		url = 'rest/usuario/';
+
+		xhr.open('POST', url, true),
+		xhr.onload = function(){
+			let obj = JSON.parse(xhr.responseText);
+			
+			if(obj.RESULTADO='OK'){
+				document.getElementById('cuerpo_registro').reset();
+				var modal = document.getElementById('modal'),
+					span = document.getElementsByClassName('close')[0];
+					modal.style.display = 'block';
+					span.onclick = function(){
+						modal.style.display='none';
+						window.location.href = 'login.html';
+					}
+
+					window.onclick = function(event){
+						if(event.target == modal){
+							modal.style.display='none';
+							window.location.href = 'login.html';
+						}
+					}
+
+			}else{
+				var contrasena = document.getElementById('contrasena_mal'),
+					usuario = document.getElementById('estado_usuario');
+
+					usuario ="";
+					contrasena.innerHTML="";
+					if(obj.CODIGO==400){
+						if(obj.DESCRIPCION=='Login no valido'){
+							usuario.innerHTML="Usuario incorrecto";
+							usuario.style.color='#FF0000';
+						}else{
+							usuario.innerHTML= "Usuario no disponible";
+							usuario.style.color='#D615F5';
+						}
+					}else{
+						if(obj.CODIGO==401){
+							contrasena.innerHTML="Las contrasenas no coinciden";
+							contrasena.stye.color='#4500DD';
+						}
+					}
+			}
+		}
+		xhr.send(frm);
+
+		return false;
+}
+
+function checkContra(event){
+	if(event.which == 32){
+		return false;
+	}
 }
